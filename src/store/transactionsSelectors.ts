f2626e1979528528882e131e3ddf9626096b74ee
shahzeb <shahzeb8285@gmail.com>
2024-05-08 16:48:16 +0530

    first commit

---
+++ src/store/transactionsSelectors.ts
@@ -0,0 +1,25 @@
+import { RootStore } from './root';
+import { TransactionEvent, Transactions } from './transactionsSlice';
+
+export const selectSuccessfulTransactions = (state: RootStore) => {
+  const successfulTransactions: Transactions = {};
+
+  try{ Object.keys(state.transactions).forEach((chainId) => {
+    const chainIdNumber = +chainId;
+    const successfulTxHashes = Object.keys(state.transactions[chainIdNumber]).filter(
+      (txHash) => state.transactions[chainIdNumber][txHash].txState === 'success'
+    );
+    successfulTransactions[chainIdNumber] = successfulTxHashes.reduce<{
+      [hash: string]: TransactionEvent;
+    }>((acc, txHash) => {
+      acc[txHash] = state.transactions[chainIdNumber][txHash];
+      return acc;
+    }, {});
+  });
+
+
+  }catch(err){
+    
+  }
+  return successfulTransactions;
+};
